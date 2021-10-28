1626f4f872ce71429bbc5bbd2c538d7e6cdf6b5b
Cathella <ncathie214@gmail.com>
2021-10-28 23:02:14 +0300

    imports and style updates

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+// task heading
+const addTitle = () => {
+  let header = document.createElement("header");
+  header.innerHTML = `
+    <h3>Today's To Do</h3>
+  `;
+  document.getElementById("todo-list").appendChild(header);
+}
+
+// task form
+const taskForm = () => {
+  let form = document.createElement("form");
+  form.innerHTML = `
+    <input type="text" placeholder="Add to your list ...">
+    <input type="submit" value="Submit" hidden>
+  `;
+  document.getElementById("todo-list").appendChild(form);
+}
+
 // function to iterate over the tasks array and populate an HTML list item element for each task
 const iterateTasks = (task) => {
   let li = document.createElement("li");
@@ -10,4 +29,11 @@ const iterateTasks = (task) => {
   document.getElementById("todo-list").appendChild(li);
 }
 
-export { iterateTasks };
+// clear completed tasks btn
+const clearBtn = () => {
+  let btn = document.createElement("button");
+  btn.innerHTML = `Clear all completed`;
+  document.getElementById("todo-list").appendChild(btn);
+}
+
+export { iterateTasks, addTitle, taskForm, clearBtn };
--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
+import _ from 'lodash';
 import './style.css';
 
 import tasks from './tasks';
-import { iterateTasks } from './app';
+import { iterateTasks, addTitle, taskForm, clearBtn } from './app';
 
-// load render the dynamically created list of tasks in the dedicated placeholder
-let header = document.createElement("header");
-header.innerHTML = `
-  <h3>Today's To Do</h3>
-`;
-document.getElementById("tasks").appendChild(header);
-
-let form = document.createElement("form");
-form.innerHTML = `
-  <input type="text" placeholder="Add to your list ...">
-  <input type="submit" value="Submit" hidden>
-`;
-document.getElementById("tasks").appendChild(form);
-
-tasks.forEach(iterateTasks);
-
-let btn = document.createElement("button");
-btn.innerHTML = `Clear all completed`;
-document.getElementById("tasks").appendChild(btn); 
+window.onload = () => {
+  addTitle(); // tasks title
+  taskForm(); // task form
+  tasks.forEach(iterateTasks); // list tasks
+  clearBtn(); // complete tasks button
+}
