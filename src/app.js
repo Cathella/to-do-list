a9ce854daee14d167f35157ea2d6aa56db54bd05
Cathella <ncathie214@gmail.com>
2021-10-28 22:51:19 +0300

    style app elements

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 // function to iterate over the tasks array and populate an HTML list item element for each task
 const iterateTasks = (task) => {
   let li = document.createElement("li");
-  li.innerHTML = `${task.description}`;
+  li.innerHTML = `
+    <div>
+      <input type="checkbox">
+      ${task.description}
+    </div>
+  `;
   document.getElementById("todo-list").appendChild(li);
 }
 
--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@ import tasks from './tasks';
 import { iterateTasks } from './app';
 
 // load render the dynamically created list of tasks in the dedicated placeholder
-tasks.forEach(iterateTasks); 
+let header = document.createElement("header");
+header.innerHTML = `
+  <h3>Today's To Do</h3>
+`;
+document.getElementById("tasks").appendChild(header);
+
+let form = document.createElement("form");
+form.innerHTML = `
+  <input type="text" placeholder="Add to your list ...">
+  <input type="submit" value="Submit" hidden>
+`;
+document.getElementById("tasks").appendChild(form);
+
+tasks.forEach(iterateTasks);
+
+let btn = document.createElement("button");
+btn.innerHTML = `Clear all completed`;
+document.getElementById("tasks").appendChild(btn); 
