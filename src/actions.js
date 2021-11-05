a0469b78fd0014c03164d90a8ebbba6356020e85
Cathella <ncathie214@gmail.com>
2021-11-05 19:51:44 +0300

    remove linter error

--- src/actions.js
+++ src/actions.js
@@ -90,7 +90,7 @@ const getTasks = (ul) => {
 };
 
 const addTask = (list, task) => {
-  task.push({ desc: list, completed: false, index: task.length + 1});
+  task.push({ desc: list, completed: false, index: task.length + 1 });
   localStorage.setItem('tasks', JSON.stringify(task));
 };
 
