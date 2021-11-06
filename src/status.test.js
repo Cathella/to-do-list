20edf1502ad247f35829a71bafc5bb91f2e2873b
Cathella <ncathie214@gmail.com>
2021-11-06 13:22:22 +0300

    add status test file

---
+++ src/status.test.js
@@ -0,0 +1,20 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {  } from './actions.js';
+
+// updating task's completed status
+describe('', () => {
+
+});
+
+// editing a task
+describe('', () => {
+
+});
+
+// clear all completed tasks
+describe('', () => {
+
+});
