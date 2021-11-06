c4e04cbd7c6b1c80310bd94513664553fb487f30
Fondem Junior <fondemjunior@yahoo.com>
2021-11-06 12:49:28 +0100

    Add editing test

--- src/status.test.js
+++ src/status.test.js
@@ -11,7 +11,19 @@ describe('', () => {
 
 // editing a task
 describe('Should edit a task', () => {
-  
+    test('should edit task from previous task to new task', () => {
+        const list = [
+          {
+            description: 'This is a task',
+            completed: false,
+            index: 1,
+          },
+        ];
+        const [task] = list;
+        const newTask = 'This is a modified task';
+        editTask(newTask, task, list);
+        expect(task.description).toMatch('This is a modified task');
+      });
 });
 
 // clear all completed tasks
